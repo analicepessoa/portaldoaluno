d6822492e8c6b7ea16b70841b94d4eedb1431e5c
analicepessoa <71182698+analicepessoa@users.noreply.github.com>
2026-08-24 17:19:44 -0300

    Separa práticas e amplia quiz da aula HTML

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // O nome do nosso "armazém" de memória (mude a versão quando atualizar o site)
-const CACHE_NAME = 'portal-educacional-v8';
+const CACHE_NAME = 'portal-educacional-v10';
 
 // Ficheiros que queremos guardar no telemóvel do aluno
 const urlsToCache = [
